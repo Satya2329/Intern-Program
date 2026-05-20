834aeab7c33b29a75b21fd1dc95e78c63957668f
Satyapriya Paikaray <satyapriyap67@gmail.com>
2026-05-20 22:03:59 +0530

    Internship Program

---
+++ Summer_Intern/PrimitiveDatatypes.java
@@ -0,0 +1,24 @@
+package Summer_Intern;
+
+public class PrimitiveDatatypes {
+
+    public static void main(String[] args) {
+        byte b = 10;
+        short s = 200;
+        int i = 5000;
+        long l = 100000L;
+        float f = 25.5f;
+        double d = 123.456;
+        char c = 'A';
+        boolean flag = true;
+
+        System.out.println("Byte Value: " + b);
+        System.out.println("Short Value: " + s);
+        System.out.println("Int Value: " + i);
+        System.out.println("Long Value: " + l);
+        System.out.println("Float Value: " + f);
+        System.out.println("Double Value: " + d);
+        System.out.println("Character Value: " + c);
+        System.out.println("Boolean Value: " + flag);
+    }
+}
---
+++ Summer_Intern/Variables_Datatype.java
@@ -0,0 +1,21 @@
+package Summer_Intern;
+
+public class Variables_Datatype {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        float b = 25.5f;
+        double c = 123.456;
+        char d = 'A';
+        boolean e = true;
+        String name = "Satya";
+
+        System.out.println("Integer Value: " + a);
+        System.out.println("Float Value: " + b);
+        System.out.println("Double Value: " + c);
+        System.out.println("Character Value: " + d);
+        System.out.println("Boolean Value: " + e);
+        System.out.println("String Value: " + name);
+    }
+}

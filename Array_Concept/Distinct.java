4aa6c7f433c08f90db40a43476b3d28463889ea9
Satya2329 <satyapriyap67@gmail.com>
2026-06-10 22:14:27 +0530

    Array Concept

--- Array_Concept/Distinct.java
+++ Array_Concept/Distinct.java
@@ -1,8 +1,35 @@
 package Array_Concept;
+import java.util.HashSet;
+import java.util.Arrays;
 
 public class Distinct {
     public static void main(String[] args) {
-        int [] arr ={1,2,4,5,6};
-        int[] arr1 ={2,5,8,9,3,1};          
+        int[] array1 = {1, 2, 3, 4, 5, 2, 1};
+        int[] array2 = {4, 5, 6, 7, 8, 6};
+
+        System.out.println("Array 1: " + Arrays.toString(array1));
+        System.out.println("Array 2: " + Arrays.toString(array2));
+        
+        printDistinctElements(array1, array2);
+    }
+
+    public static void printDistinctElements(int[] arr1, int[] arr2) {
+       
+        HashSet<Integer> distinctSet = new HashSet<>();
+
+        for (int num : arr1) {
+            distinctSet.add(num);
+        }
+
+        for (int num : arr2) {
+            distinctSet.add(num);
+        }
+
+       
+        System.out.println("Distinct elements present in both arrays:");
+        for (int num : distinctSet) {
+            System.out.print(num + " ");
+        }
+        System.out.println(); // For a clean newline
     }
 }

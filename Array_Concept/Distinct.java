654d554c9509f29a4d967794069619ae0b1a2cf6
Satya2329 <satyapriyap67@gmail.com>
2026-06-10 22:13:47 +0530

    Array Concept

---
+++ Array_Concept/Distinct.java
@@ -0,0 +1,8 @@
+package Array_Concept;
+
+public class Distinct {
+    public static void main(String[] args) {
+        int [] arr ={1,2,4,5,6};
+        int[] arr1 ={2,5,8,9,3,1};          
+    }
+}

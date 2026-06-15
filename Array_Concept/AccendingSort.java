3933e3176fc8299079e62dd3a5a1483d164881e1
Satya2329 <satyapriyap67@gmail.com>
2026-06-15 22:31:55 +0530

    Array Concept

---
+++ Array_Concept/AccendingSort.java
@@ -0,0 +1,23 @@
+package Array_Concept; 
+import java.util.Arrays;
+
+public class AccendingSort {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 8, 7, 1, 3};
+        int temp = 0;
+        
+        System.out.println("Original array: " + Arrays.toString(numbers));
+        
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] > numbers[j]) {
+                    temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted array (Ascending): " + Arrays.toString(numbers));
+    }
+}
+
